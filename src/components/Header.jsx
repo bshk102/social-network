3d8e74568872423736ce1e23b7722ed83c3e1067
Your God <ass@hole>
2020-09-10 12:39:34 +0500

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <img src="https://autodoktor.com.ua/wp-content/uploads/Logo/Total-logo-earth.png" />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = (props) => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://media.resources.festicket.com/image/412x150/smart/filters:quality(70)/www/magazine/Techno_B_qdqEYIm.jpg" />
+            </div>
+            <div>
+                avatar + descr
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
