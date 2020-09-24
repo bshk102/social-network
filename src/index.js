4896424070cdfc3c5ca7173b9f5b1957f58297e5
bshk102 <xartem.kx@gmail.com>
2020-09-24 21:47:20 +0500

    router-dom

--- src/index.js
+++ src/index.js
@@ -5,20 +5,27 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import Music from './components/Music/Music';
+import {Route, BrowserRouter} from 'react-router-dom';
 
 
 const App = (props) => {
     return (
-        <div>
+        <BrowserRouter>
             <div className="app-wrapper">
                 <Header />
                 <Navbar />
                 <div className="app-wrapper-content">
-                    {/* <Dialogs /> */}
-                    <Profile />
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
                 </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 };
 
