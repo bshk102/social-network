8a8fc82de76bdcd5877f8b07979ec704439417b7
Your God <ass@hole>
2020-10-01 15:46:18 +0500

    Lesson 31, added button to dialogs; onClick, createRef on btns

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,13 +8,25 @@ const Dialogs = (props) => {
     let dialogsHTML = props.dialogs.dialogsData.map(el => <DialogsItem name={el.name} id={el.id} />);
     let messagesHTML = props.dialogs.messagesData.map(el => <Message message={el.message} />);
 
+    const textMessage = React.createRef();
+
+    const sendMessage = () => {
+        console.log(textMessage.current.value);
+    };
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsHTML}
             </div>
-            <div className={s.messages}>
-                {messagesHTML}
+            <div>
+                <div className={s.messages}>
+                    {messagesHTML}
+                </div>
+                <div className={s.sendMessage}>
+                    <textarea ref={textMessage} cols="35" rows="4"></textarea>
+                    <button onClick={sendMessage}>send</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
@@ -6,12 +6,19 @@ const MyPosts = (props) => {
 
     let postsHTML = props.postsData.map(el => <Post message={el.message} likesCount={el.likesCount} />);
 
+    const postText = React.createRef();
+
+    const addPost = () => {
+        console.log(postText.current.value);
+        postText.current.value = '';
+    };
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea name="" id="" cols="20" rows="4"></textarea>
-                <button>Add Post</button>
+                <textarea ref={postText} cols="20" rows="4"></textarea>
+                <button onClick={addPost}>Add Post</button>
             </div>
             <div className={s.posts}>
                 {postsHTML}
