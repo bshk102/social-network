154ef45f3a7dc3d5602d81ae7f7b7f0f129f0323
Your God <ass@hole>
2020-10-01 11:37:35 +0500

    decorate a dialogs little bit

--- src/components/Dialogs/DialogsItem/DialogsItem.jsx
+++ src/components/Dialogs/DialogsItem/DialogsItem.jsx
@@ -4,8 +4,11 @@ import { NavLink } from 'react-router-dom';
 
 const DialogsItem = (props) => {
     return (
-        <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        <div className={s.dialog}>
+            <NavLink to={`/dialogs/${props.id}`}>
+                {props.name}
+                <img src="https://www.vippng.com/png/full/416-4161690_empty-profile-picture-blank-avatar-image-circle.png" alt=""/>               
+            </NavLink>
         </div>
     );
 };
